3b52917ece92d97b13fc5a1023ab5955aa61b513
cairo <cairoqueiros@edu.unifor.br>
2025-10-20 17:58:32 -0300

    ajutando buscas de mecanica e de temas

--- view/src/api/mecanica.ts
+++ view/src/api/mecanica.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const getMecanicas = async () => {
   try {
-    const response = await axios.get('https://localhost:7226/Mecanica');
+    const response = await axios.get(`${baseURL}/Mecanica`);
     return response.data; // array de { id, nmMecanica }
   } catch (error) {
     console.error('Erro ao buscar mecânicas:', error);
--- view/src/api/tema.ts
+++ view/src/api/tema.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const getTemas = async () => {
   try {
-    const response = await axios.get('https://localhost:7226/Tema');
+    const response = await axios.get(`${baseURL}/Tema`);
     return response.data; // array de { id, nmTema }
   } catch (error) {
     console.error('Erro ao buscar temas:', error);
