fd5a204463519c94660e0f5bdf6cc94f5ce17aa6
Leonardo Batista <104659313+LeoBat00@users.noreply.github.com>
2025-11-12 18:09:33 -0300

    ajustes enums

--- view/src/enums/categorias.ts
+++ view/src/enums/categorias.ts
@@ -1,27 +1,25 @@
-export const CATEGORIAS_MAP = {
-  1: "Miniaturas",
-  2: "Jogo Assimétrico",
-  3: "Dungeon Crawler",
-  4: "Jogo de Dados",
-  5: "Expansão ou Suplemento",
-  6: "Livro-jogo",
-  7: "4x",
-  8: "Jogo de Cartas",
-  9: "Jogo de Guerra",
-  10: "Integrado com Aplicativo",
-  11: "Estratégia Abstrata",
-  12: "Quebra-Cabeça",
-  13: "Colecionável",
-  14: "Jogo de Entrada",
-  15: "Carteado",
-  16: "Jogo Festivo",
-  17: "Destreza",
-  18: "Imprima e Jogue",
-  19: "Trivia",
+export const CATEGORIAS = {
+  MINIATURAS: { id: 1, descricao: "Miniaturas" },
+  JOGO_ASSIMETRICO: { id: 2, descricao: "Jogo Assimétrico" },
+  DUNGEON_CRAWLER: { id: 3, descricao: "Dungeon Crawler" },
+  JOGO_DE_DADOS: { id: 4, descricao: "Jogo de Dados" },
+  EXPANSAO_OU_SUPLEMENTO: { id: 5, descricao: "Expansão ou Suplemento" },
+  LIVRO_JOGO: { id: 6, descricao: "Livro-jogo" },
+  QUATRO_X: { id: 7, descricao: "4x" },
+  JOGO_DE_CARTAS: { id: 8, descricao: "Jogo de Cartas" },
+  JOGO_DE_GUERRA: { id: 9, descricao: "Jogo de Guerra" },
+  INTEGRADO_COM_APLICATIVO: { id: 10, descricao: "Integrado com Aplicativo" },
+  ESTRATEGIA_ABSTRATA: { id: 11, descricao: "Estratégia Abstrata" },
+  QUEBRA_CABECA: { id: 12, descricao: "Quebra-Cabeça" },
+  COLECIONAVEL: { id: 13, descricao: "Colecionável" },
+  JOGO_DE_ENTRADA: { id: 14, descricao: "Jogo de Entrada" },
+  CARTEADO: { id: 15, descricao: "Carteado" },
+  JOGO_FESTIVO: { id: 16, descricao: "Jogo Festivo" },
+  DESTREZA: { id: 17, descricao: "Destreza" },
+  IMPRIMA_E_JOGUE: { id: 18, descricao: "Imprima e Jogue" },
+  TRIVIA: { id: 19, descricao: "Trivia" },
 } as const;
 
-export type CategoriaId = keyof typeof CATEGORIAS_MAP;
-
-export const CATEGORIAS_LIST = Object.entries(CATEGORIAS_MAP).map(
-  ([id, nome]) => ({ id: Number(id), nome })
-);
+export type CategoriaKey = keyof typeof CATEGORIAS;
+export type CategoriaInfo = (typeof CATEGORIAS)[CategoriaKey];
+export const CATEGORIAS_LIST = Object.values(CATEGORIAS);
